45bb123abc72e0e6586b2ee09cc155c7e2bacb27
Hazael Lopez <elhazael134@gmail.com>
2023-10-17 23:00:58 -0600

    Create Product DTO

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/');
+  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     })
-  );
+   );
 
   await app.listen(process.env.PORT);
 }
