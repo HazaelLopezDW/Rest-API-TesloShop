76ee51082672f181bbf9826ac30ecb37f6c38b48
Hazael Lopez <elhazael134@gmail.com>
2023-10-17 22:26:43 -0600

    Entidad sin relaciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -11,6 +11,33 @@ export class Product {
      })
     title: string;
 
+    @Column('numeric', {
+        default: 0
+    })
+    price: number;
 
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
 
 }
