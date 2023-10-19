518f21fefba3467fc016bd3ab6194bee01aa98f2
Hazael Lopez <elhazael134@gmail.com>
2023-10-18 22:55:06 -0600

    QueryBuilder

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -53,10 +53,13 @@ export class ProductsService {
     if(isUUID(term)){
       product = await this.productRepository.findOneBy({ id: term });
     }else {
-      product = await this.productRepository.findOneBy({ slug: term });
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder.where(`UPPER(title) =:title or slug =:slug`, {
+        title: term.toUpperCase(),
+        slug: term.toLocaleLowerCase(),
+      }).getOne();
     }
 
-    // const product = await this.productRepository.findOneBy({term});
 
     if(!product) 
       throw new NotFoundException(`Product with Id ${term} not found`);
