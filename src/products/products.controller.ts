faba7c48d58e7dc26a6ad9121e151496137b38f1
Hazael Lopez <elhazael134@gmail.com>
2023-10-19 09:39:48 -0600

    Update en TypeORM

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -31,8 +31,11 @@ export class ProductsController {
 
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id') id: string, 
+    @Body() updateProductDto: UpdateProductDto
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -68,9 +68,25 @@ export class ProductsService {
   }
 
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+
+    const product = await this.productRepository.preload({ 
+      id,
+      ...updateProductDto
+    });
+
+    if(!product) throw new NotFoundException(`Product with Id: ${id} not found`);
+
+    try{
+      await this.productRepository.save(product);
+      return product;
+    }catch(error){
+      this.handleDBExceptions(error);
+    }
   }
+
+
 
   async remove(id: string) {
     const product  = await this.findOne(id);
