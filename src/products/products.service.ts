01377935b1575e67586c97e1c9d6508a0d35c05e
Hazael Lopez <elhazael134@gmail.com>
2023-10-20 23:09:04 -0600

    Transacciones

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -107,12 +107,28 @@ export class ProductsService {
 
     //* Create Query Runner
     const queryRunner = this.dataSource.createQueryRunner();
-    
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
     try{
-      await this.productRepository.save(product);
-      return product;
+      if(images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+        product.images = images.map(
+          image => this.productImageRepository.create({url: image})
+        )
+      }
+
+      await queryRunner.manager.save(product);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+
+      return this.findOnePlain(id);
+
     }catch(error){
+
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+
       this.handleDBExceptions(error);
     }
   }
