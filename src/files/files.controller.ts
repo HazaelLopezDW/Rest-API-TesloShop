e76e05333ae7f3caadffb75fdeced903744162f0
Hazael Lopez <elhazael134@gmail.com>
2023-10-23 10:00:54 -0600

    Subir un archivo al backend

---
+++ src/files/files.controller.ts
@@ -0,0 +1,18 @@
+import { Controller,  Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadProductImage(
+    @UploadedFile() file: Express.Multer.File,
+  ){
+    return file;
+  }
+
+}
