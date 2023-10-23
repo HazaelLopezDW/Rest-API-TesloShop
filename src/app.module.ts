b4215f666ddf5a5869ca32c2ea739fc84b13443d
Hazael Lopez <elhazael134@gmail.com>
2023-10-23 12:27:11 -0600

    Otras formas de desplegar archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -22,6 +24,11 @@ import { FilesModule } from './files/files.module';
       autoLoadEntities: true,
       synchronize: true, 
     }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+    }),
+
     ProductsModule,
     CommonModule,
     SeedModule,
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('bootstrap')
 
   //* Prefijos de nuestras rutas
   app.setGlobalPrefix('api');
@@ -18,5 +19,6 @@ async function bootstrap() {
 
    // * Llamamos el listen para que nuestro servidor corra en el puerto especificado 
   await app.listen(process.env.PORT);
+  logger.log(`App running on port: ${process.env.PORT}`);
 }
 bootstrap();
