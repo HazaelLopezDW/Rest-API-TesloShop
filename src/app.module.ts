3cc95d06883461f43ea42fff00ce5bca1d8aa8b3
Hazael Lopez <elhazael134@gmail.com>
2023-10-23 22:45:20 -0600

    Entidad de Usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -34,6 +35,7 @@ import { join } from 'path';
     CommonModule,
     SeedModule,
     FilesModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('users')
+export class User {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true
+    })
+    email: string;
+
+    @Column('text')
+    password: string;
+
+    @Column('text')
+    fullName: string;
+
+    @Column('bool', {
+        unique: true
+    })
+    isActive: boolean;
+
+    @Column('text', {
+        array: true,
+        default: ['user']
+    })
+    roles: string[];
+}
