d63c21210f2a41bb873bf66c9d32f1bb9baa4ddb
Hazael Lopez <elhazael134@gmail.com>
2023-10-24 20:34:52 -0600

    Private Route - General

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -14,6 +15,16 @@ export class AuthController {
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard()) //? Validamos que tengamos un token y este activo
+  testingPrivateRoute() {
+    return {
+      ok: true,
+      msg: 'Hola mundo private',
+      user: { name: 'Hazael'}
+    }
   }
 
 }
