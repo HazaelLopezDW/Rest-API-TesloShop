8e1574a7c780c21f5e929005ee7e3fa39b8d9ea8
Hazael Lopez <elhazael134@gmail.com>
2023-10-25 10:25:30 -0600

    Custom Guard y Custom Decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from './entities/user.entity';
 import { GetUser, RawHeaders } from './decorators';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -22,13 +23,12 @@ export class AuthController {
   @Get('private')
   @UseGuards(AuthGuard()) //? Validamos que tengamos un token y este activo
   testingPrivateRoute(
-    // @Req() request: Express.Request,
     @GetUser() user: User,
     @GetUser('email') userEmail: string,
     @RawHeaders() rawHeaders: string[]
   ) {
 
-    // console.log(request)
+
 
     return {
       ok: true,
@@ -36,6 +36,18 @@ export class AuthController {
       user,
       userEmail,
       rawHeaders
+    }
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(
+    @GetUser() user: User
+  ){
+    return {
+      ok: true,
+      user
     }
   }
 
