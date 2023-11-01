bfbb683ec7f29986c1f0290675a9ca429609fb46
Hazael Lopez <elhazael134@gmail.com>
2023-11-01 10:57:44 -0600

    Documentar DTOs

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 
 export class CreateUserDto {
 
+    @ApiProperty()
     @IsString()
     @IsEmail()
     email: string;
 
+    @ApiProperty()
     @IsString()
     @MinLength(6)
     @MaxLength(50)
@@ -16,6 +19,7 @@ export class CreateUserDto {
     })
     password: string;
 
+    @ApiProperty()
     @IsString()
     @MinLength(2)
     fullName: string;
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,42 +1,56 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, MinLength, IsNumber, IsPositive, IsOptional, IsInt,
          IsArray, IsIn} from "class-validator";
 
 export class CreateProductDto {
 
+    @ApiProperty({
+        description: 'Product title (unique)',
+        nullable: false,
+        minLength: 1
+    })
     @IsString()
     @MinLength(1)
     title: string;
 
+    @ApiProperty()
     @IsNumber()
     @IsPositive()
     @IsOptional()
     price?: number;
 
+    @ApiProperty()
     @IsString()
     @IsOptional()
     description?: string;
 
+    @ApiProperty()
     @IsString()
     @IsOptional()
     slug?: string;
 
+    @ApiProperty()
     @IsInt()
     @IsPositive()
     @IsOptional()
     stock?: number;
 
+    @ApiProperty()
     @IsString({ each: true })
     @IsArray()
     sizes: string[];
 
+    @ApiProperty()
     @IsIn(['men', 'women', 'kid', 'unisex'])
     gender: string;
 
+    @ApiProperty()
     @IsString({each: true})
     @IsArray()
     @IsOptional()
     tags: string[];
 
+    @ApiProperty()
     @IsString({each: true})
     @IsArray()
     @IsOptional()
