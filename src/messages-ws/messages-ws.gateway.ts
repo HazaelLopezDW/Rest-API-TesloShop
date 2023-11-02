bbac335a7d6ee4f887cc09c0b7808b256da2e897
Hazael Lopez <elhazael134@gmail.com>
2023-11-02 10:54:10 -0600

    Server - Escuchar conexiones y desconexiones

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,17 +1,24 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 import { Get } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
 
-  constructor(private readonly messagesWsService: MessagesWsService) {
-      
+  constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket) {
+    console.log('Cliente Conectado:', client.id);
   }
+
+  handleDisconnect(client: Socket) {
+    console.log('Cliente Desconectado:', client.id);
+  }
+
   
-  @Get()
-  getMethod() {
-    return `Hola mundo`
-  }
+
+
+ 
 
 }
